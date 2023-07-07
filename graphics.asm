68aa0cc20f2cfbe1bfcf45f816d92f626b39f9c0
Dan M <daniel@thewindmills.com.au>
2023-07-07 16:28:26 +1000

    Removed obj memory as we're not using it

--- graphics.asm
+++ graphics.asm
@@ -1,29 +1,5 @@
 SECTION "Grahpics Functions", ROM0
 
-
-; FUNCTION: WriteObjectPalettes
-;
-; Writes a group of bytes to rOCPD
-;
-; MODIFIES:
-;
-; `a`, `bc`, `de`
-;
-; @param de: Address for the palettes
-; @param bc: Length of the palette data
-WriteObjectPalettes:
-    ld      a, %10000000
-    ldh     [rOCPS], a
-.WriteObjectPalettesLoop
-    ld      a, [de]
-    ldh     [rOCPD], a
-    inc     de
-    dec     bc
-    ld      a, b
-    or      a, c
-    jp      nz, .WriteObjectPalettesLoop
-    ret
-
 ; FUNCTION: WriteBgPalettes
 ;
 ; Writes a group of bytes to rBCPD
